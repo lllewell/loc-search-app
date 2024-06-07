02d2b3b3fb9d54c2399763042f4217ad6e3a0f14
Alien <lmllewellyn@gmail.com>
2024-06-06 20:18:42 -0400

    added search form and js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const qInput = document.querySelector('#q');
 const searchForm = document.querySelector('#search-form');
+const resultList = document.querySelector('#results');
 
 fetch('https://www.loc.gov/film-and-videos/?q=dog&fo=json')
     .then(function (response){
@@ -13,28 +14,32 @@ fetch('https://www.loc.gov/film-and-videos/?q=dog&fo=json')
         console.log(err);
     });
 
-    fetch('https://www.loc.gov/search/?q=baseball&fo=json')
-    .then(function (response){
-        return response.json();
-    })
-    .then(function (data) {
-        console.log('SEARCH');
-        console.log(data);
-    })
-    .catch(function (err) {
-        console.log(err);
-    });
 
-    searchForm.addEventListener('submit', function () {
-        fetch('https://www.loc.gov/search/?q=baseball&fo=json')
-    .then(function (response){
-        return response.json();
-    })
-    .then(function (data) {
-        console.log('SEARCH');
-        console.log(data);
-    })
-    .catch(function (err) {
-        console.log(err);
+    searchForm.addEventListener('submit', function (event) {
+        event.preventDefault
+
+        const q = qInput.ariaValueMax.trim();
+        fetch('https://www.loc.gov/search/?q=' + q + '&fo=json')
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log('SEARCH');
+          console.log(data);
+  
+          for (let result of data.results) {
+            const columnEl = document.createElement('div');
+            const cardEl = document.createElement('div');
+  
+            columnEl.className = 'col-12';
+            cardEl.className = 'card p-3 m-3';
+            cardEl.textContent = result.title;
+  
+            columnEl.appendChild(cardEl);
+            resultList.appendChild(columnEl);
+          }
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
     });
-    })
